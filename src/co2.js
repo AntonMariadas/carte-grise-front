3888ec1618bd7c4e648186782cc022949cf0cbdc
Anton M <mariadasanton27@gmail.com>
2021-03-24 14:20:53 +0100

    fin de creation données taxe co2

--- src/co2.js
+++ src/co2.js
@@ -17,444 +17,421 @@ export const co2 = [
     {
         "id": 4,
         "taux": "135 g",
-        "taxe": 50
+        "taxe": 100
     },
     {
         "id": 5,
         "taux": "136 g",
-        "taxe": 50
+        "taxe": 125
     },
     {
         "id": 6,
         "taux": "137 g",
-        "taxe": 50
+        "taxe": 150
     },
     {
         "id": 7,
         "taux": "138 g",
-        "taxe": 50
+        "taxe": 170
     },
     {
         "id": 8,
         "taux": "139 g",
-        "taxe": 50
+        "taxe": 190
     },
     {
         "id": 9,
         "taux": "140 g",
-        "taxe": 50
+        "taxe": 210
     },
     {
         "id": 10,
         "taux": "141 g",
-        "taxe": 50
+        "taxe": 230
     },
     {
         "id": 11,
         "taux": "142 g",
-        "taxe": 50
+        "taxe": 240
     },
     {
         "id": 12,
         "taux": "143 g",
-        "taxe": 50
+        "taxe": 260
     },
     {
         "id": 13,
         "taux": "144 g",
-        "taxe": 50
+        "taxe": 280
     },
     {
         "id": 14,
         "taux": "145 g",
-        "taxe": 50
+        "taxe": 310
     },
     {
         "id": 15,
         "taux": "146 g",
-        "taxe": 50
+        "taxe": 330
     },
     {
         "id": 16,
         "taux": "147 g",
-        "taxe": 50
+        "taxe": 360
     },
     {
         "id": 17,
         "taux": "148 g",
-        "taxe": 50
+        "taxe": 400
     },
     {
         "id": 18,
         "taux": "149 g",
-        "taxe": 50
+        "taxe": 450
     },
     {
         "id": 19,
         "taux": "150 g",
-        "taxe": 50
+        "taxe": 540
     },
     {
         "id": 20,
         "taux": "151 g",
-        "taxe": 50
+        "taxe": 650
     },
     {
         "id": 21,
         "taux": "152 g",
-        "taxe": 50
+        "taxe": 740
     },
     {
         "id": 22,
         "taux": "153 g",
-        "taxe": 50
+        "taxe": 818
     },
     {
         "id": 23,
         "taux": "154 g",
-        "taxe": 50
+        "taxe": 898
     },
     {
         "id": 24,
         "taux": "155 g",
-        "taxe": 50
+        "taxe": 983
     },
     {
         "id": 25,
         "taux": "156 g",
-        "taxe": 50
+        "taxe": 1074
     },
     {
         "id": 26,
         "taux": "157 g",
-        "taxe": 50
+        "taxe": 1172
     },
     {
         "id": 27,
         "taux": "158 g",
-        "taxe": 50
+        "taxe": 1276
     },
     {
         "id": 28,
         "taux": "159 g",
-        "taxe": 50
+        "taxe": 1386
     },
     {
         "id": 29,
         "taux": "160 g",
-        "taxe": 50
+        "taxe": 1504
     },
     {
         "id": 30,
         "taux": "161 g",
-        "taxe": 50
+        "taxe": 1629
     },
     {
         "id": 31,
         "taux": "162 g",
-        "taxe": 50
+        "taxe": 1761
     },
     {
         "id": 32,
         "taux": "163 g",
-        "taxe": 50
+        "taxe": 1901
     },
     {
         "id": 33,
         "taux": "164 g",
-        "taxe": 50
+        "taxe": 2049
     },
     {
         "id": 34,
         "taux": "165 g",
-        "taxe": 50
+        "taxe": 2205
     },
     {
         "id": 35,
         "taux": "166 g",
-        "taxe": 50
+        "taxe": 2370
     },
     {
         "id": 36,
         "taux": "167 g",
-        "taxe": 50
+        "taxe": 2544
     },
     {
         "id": 37,
         "taux": "168 g",
-        "taxe": 50
+        "taxe": 2726
     },
     {
         "id": 38,
         "taux": "169 g",
-        "taxe": 50
+        "taxe": 2918
     },
     {
         "id": 39,
         "taux": "170 g",
-        "taxe": 50
+        "taxe": 3119
     },
     {
         "id": 40,
         "taux": "171 g",
-        "taxe": 50
+        "taxe": 3331
     },
     {
         "id": 41,
         "taux": "172 g",
-        "taxe": 50
+        "taxe": 3552
     },
     {
         "id": 41,
         "taux": "173 g",
-        "taxe": 50
+        "taxe": 3784
     },
     {
         "id": 43,
         "taux": "174 g",
-        "taxe": 50
+        "taxe": 4026
     },
     {
         "id": 44,
         "taux": "175 g",
-        "taxe": 50
+        "taxe": 4279
     },
     {
         "id": 45,
         "taux": "176 g",
-        "taxe": 50
+        "taxe": 4543
     },
     {
         "id": 46,
         "taux": "177 g",
-        "taxe": 50
+        "taxe": 4818
     },
     {
         "id": 47,
         "taux": "178 g",
-        "taxe": 50
+        "taxe": 5105
     },
     {
         "id": 48,
         "taux": "179 g",
-        "taxe": 50
+        "taxe": 5404
     },
     {
         "id": 49,
         "taux": "180 g",
-        "taxe": 50
+        "taxe": 5175
     },
     {
         "id": 50,
         "taux": "181 g",
-        "taxe": 50
+        "taxe": 6039
     },
     {
         "id": 51,
         "taux": "182 g",
-        "taxe": 50
+        "taxe": 6370
     },
     {
         "id": 52,
         "taux": "183 g",
-        "taxe": 50
+        "taxe": 6724
     },
     {
         "id": 53,
         "taux": "184 g",
-        "taxe": 50
+        "taxe": 7086
     },
     {
         "id": 54,
         "taux": "185 g",
-        "taxe": 50
+        "taxe": 7462
     },
     {
         "id": 55,
         "taux": "186 g",
-        "taxe": 50
+        "taxe": 7851
     },
     {
         "id": 56,
         "taux": "187 g",
-        "taxe": 50
+        "taxe": 8254
     },
     {
         "id": 57,
         "taux": "188 g",
-        "taxe": 50
+        "taxe": 8671
     },
     {
         "id": 58,
         "taux": "189 g",
-        "taxe": 50
+        "taxe": 9103
     },
     {
         "id": 59,
         "taux": "190 g",
-        "taxe": 50
+        "taxe": 9550
     },
     {
         "id": 60,
         "taux": "191 g",
-        "taxe": 50
+        "taxe": 10011
     },
     {
         "id": 61,
         "taux": "192 g",
-        "taxe": 50
+        "taxe": 10488
     },
     {
         "id": 62,
         "taux": "193 g",
-        "taxe": 50
+        "taxe": 12012
     },
     {
         "id": 63,
         "taux": "194 g",
-        "taxe": 50
+        "taxe": 12552
     },
     {
         "id": 64,
         "taux": "195 g",
-        "taxe": 50
+        "taxe": 13109
     },
     {
         "id": 65,
         "taux": "196 g",
-        "taxe": 50
+        "taxe": 13682
     },
     {
         "id": 66,
         "taux": "197 g",
-        "taxe": 50
+        "taxe": 14273
     },
     {
         "id": 67,
         "taux": "198 g",
-        "taxe": 50
+        "taxe": 14881
     },
     {
         "id": 68,
         "taux": "199 g",
-        "taxe": 50
+        "taxe": 0
     },
     {
         "id": 69,
         "taux": "200 g",
-        "taxe": 50
+        "taxe": 15506
     },
     {
         "id": 70,
         "taux": "201 g",
-        "taxe": 50
+        "taxe": 16149
     },
     {
         "id": 71,
         "taux": "202 g",
-        "taxe": 50
+        "taxe": 16810
     },
     {
         "id": 72,
         "taux": "203 g",
-        "taxe": 50
+        "taxe": 17490
     },
     {
         "id": 73,
         "taux": "204 g",
-        "taxe": 50
+        "taxe": 18188
     },
     {
         "id": 74,
         "taux": "205 g",
-        "taxe": 50
+        "taxe": 18905
     },
     {
         "id": 75,
         "taux": "206 g",
-        "taxe": 50
+        "taxe": 19641
     },
     {
         "id": 76,
         "taux": "207 g",
-        "taxe": 50
+        "taxe": 20396
     },
     {
         "id": 77,
         "taux": "208 g",
-        "taxe": 50
+        "taxe": 21171
     },
     {
         "id": 78,
         "taux": "209 g",
-        "taxe": 50
+        "taxe": 21966
     },
     {
         "id": 79,
         "taux": "210 g",
-        "taxe": 50
+        "taxe": 22781
     },
     {
         "id": 80,
         "taux": "211 g",
-        "taxe": 50
+        "taxe": 23616
     },
     {
         "id": 81,
         "taux": "212 g",
-        "taxe": 50
+        "taxe": 24472
     },
     {
         "id": 82,
         "taux": "213 g",
-        "taxe": 50
+        "taxe": 25349
     },
     {
         "id": 83,
         "taux": "214 g",
-        "taxe": 50
+        "taxe": 26247
     },
     {
         "id": 84,
         "taux": "215 g",
-        "taxe": 50
+        "taxe": 27166
     },
     {
         "id": 85,
         "taux": "216 g",
-        "taxe": 50
+        "taxe": 28107
     },
     {
         "id": 86,
         "taux": "217 g",
-        "taxe": 50
+        "taxe": 29070
     },
     {
         "id": 87,
         "taux": "218 g",
-        "taxe": 50
+        "taxe": 30000
     },
-    {
-        "id": 88,
-        "taux": "Supérieur à 218 g",
-        "taxe": 50
-    },
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ]
